75d189c6e1e5fcef22e4bad06e8388f3848a108e
manu <manuel.baden@hotmail.de>
2024-03-04 17:58:32 +0100

    add DateTimePicker

---
+++ src/components/DateTimePicker.jsx
@@ -0,0 +1,9 @@
+import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
+
+const DateTimePicker = () => {
+    return (
+        <StaticDateTimePicker orientation="landscape" />
+    );
+}
+
+export default DateTimePicker
