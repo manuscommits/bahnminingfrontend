c4648abe6d41c8320fa1d924a385c19727b3be31
manu <manuel.baden@hotmail.de>
2024-03-06 00:29:12 +0100

    use store for anfragezeitpunkt

--- src/components/DateTimePicker.jsx
+++ src/components/DateTimePicker.jsx
@@ -1,21 +1,33 @@
 import { DatePicker, TimePicker } from '@mui/x-date-pickers';
 import Stack from '@mui/material/Stack';
-import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setAnfrageZeitpunkt } from '../redux/verbindungsanfrageSlice';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 
 const DateTimePicker = () => {
-    const [value, setValue] = useState();
+    const dispatch = useDispatch()
+    const [abfahrtsdatum, setAbfahrtsdatum] = useState()
+    const [abfahrtszeit, setAbfahrtszeit] = useState()
+    console.log(abfahrtsdatum, abfahrtszeit)
+
+    useEffect(() => {
+        if (abfahrtsdatum && abfahrtszeit) {
+            dispatch(setAnfrageZeitpunkt(abfahrtsdatum + "T" + abfahrtszeit))
+        }
+    }, [abfahrtsdatum, abfahrtszeit])
 
     return (
         <Stack direction="row" spacing={3} style={{ padding: 10 }}>
             <DatePicker
                 label="Abfahrtsdatum"
                 format="DD.MM.YYYY"
+                onChange={(newValue) => setAbfahrtsdatum(dayjs(newValue).format("YYYY-MM-DD"))}
             />
             <TimePicker
                 label="Uhrzeit"
-                value={value}
-                onChange={setValue}
                 ampm={false}
+                onChange={(newValue) => setAbfahrtszeit(dayjs(newValue).format("HH:mm:00"))}
             />
         </Stack>
     )
