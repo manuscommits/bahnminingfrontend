8f97ea64bfff9736120fcda3777f0908b4be3a1a
manu <manuel.baden@hotmail.de>
2024-03-05 20:41:19 +0100

    add fahrplan angebote fetch function

---
+++ src/utils/fetchFahrplanAngebote.js
@@ -0,0 +1,55 @@
+import fetchWithProxy from "./bahnProxy"
+
+const url = "https://www.bahn.de/web/api/angebote/fahrplan"
+
+const fetchFahrplanAngebote = (abfahrtsHalt, ankunftsHalt, anfrageZeitpunkt, nurDirektverbindungen, pagingReference) => {
+    const body = {
+        "abfahrtsHalt": abfahrtsHalt,
+        "anfrageZeitpunkt": anfrageZeitpunkt,
+        "ankunftsHalt": ankunftsHalt,
+        "ankunftSuche": "ABFAHRT",
+        "klasse": "KLASSE_2",
+        "pagingReference": pagingReference ? pagingReference : undefined,
+        "maxUmstiege": nurDirektverbindungen ? 0 : undefined,
+        "produktgattungen": [
+            "ICE",
+            "EC_IC",
+            "IR",
+            "REGIONAL",
+            "SBAHN",
+            "BUS",
+            "SCHIFF",
+            "UBAHN",
+            "TRAM",
+            "ANRUFPFLICHTIG"
+        ],
+        "reisende": [
+            {
+                "typ": "ERWACHSENER",
+                "ermaessigungen": [
+                    {
+                        "art": "KEINE_ERMAESSIGUNG",
+                        "klasse": "KLASSENLOS"
+                    }
+                ],
+                "alter": [],
+                "anzahl": 1
+            }
+        ],
+        "rueckfahrtAnfrageFolgt": false,
+        "schnelleVerbindungen": true,
+        "sitzplatzOnly": false,
+        "bikeCarriage": false,
+        "reservierungsKontingenteVorhanden": false
+    }
+    const options = {
+        "method": "POST",
+        "body": JSON.stringify(body),
+        "headers": { 'Content-Type': 'application/json' }
+    }
+
+    return fetchWithProxy(url, options)
+        .then(response => response.json())
+}
+
+export default fetchFahrplanAngebote
