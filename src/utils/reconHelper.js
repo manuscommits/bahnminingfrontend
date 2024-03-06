e49437fdbbfb232ca099c2ca3d0ee52f9708ad2c
manu <manuel.baden@hotmail.de>
2024-03-07 00:20:25 +0100

    fix recon functions

--- src/utils/reconHelper.js
+++ src/utils/reconHelper.js
@@ -1,12 +1,15 @@
 import dayjs from "dayjs"
 
 const extractVerbindungsabschnitteRecon = (rawRecon) => {
-    const splitter = '$$1$$$$$$'
-    if (rawRecon.endsWith(splitter)) {
+    const splitter = /(\$\$[0-9]+\$\$\$\$\$\$)/g
+    if (rawRecon.endsWith("$$$$$$")) {
         return rawRecon
     }
     const verbindungsabschnitte = rawRecon.split(splitter).slice(0, -1)
-    const joinedVerbindungsabschnitte = verbindungsabschnitte.join(splitter) + splitter
+    const joinedVerbindungsabschnitte = verbindungsabschnitte.join("")
+    if (!joinedVerbindungsabschnitte.endsWith("$$$$$$")) {
+        console.log("Recon: " + rawRecon + "  is not parseable")
+    }
     return joinedVerbindungsabschnitte
 }
 
@@ -23,24 +26,22 @@ const parseRecon = (rawRecon) => {
     const recon = extractVerbindungsabschnitteRecon(rawRecon)
     try {
         const verbindungsabschnitte = recon
-            .split('$$1$$$$$$')
+            .split(/\$\$[0-9]+\$+\$/)
             .filter((x) => x)
             .map((abschnitt) => {
                 const dataList = abschnitt.split('@')
                 const abfahrtsHalt = dataList[1].slice(2)
                 const ankunftsHalt = dataList[5].slice(2)
                 const timesAndTrain = dataList[8].split('$').filter((x) => x)
-                console.log(timesAndTrain)
                 const abfahrtsZeit = compactFormatToDate(timesAndTrain[0])
                 const ankunftsZeit = compactFormatToDate(timesAndTrain[1])
-                const zug = timesAndTrain[2].replace(/\s+/g, ' ')
+                const zug = timesAndTrain[2] ? timesAndTrain[2].replace(/\s+/g, ' ') : null
                 return { abfahrtsHalt, ankunftsHalt, abfahrtsZeit, ankunftsZeit, zug }
             })
         return verbindungsabschnitte
     } catch (err) {
         console.log("Error while parsing recon: " + recon + ". Error message " + err)
     }
-
 }
 
 export { extractVerbindungsabschnitteRecon, parseRecon }
