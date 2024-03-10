4bfd8f01cc42b7e7d0908fc0548e9d54d8036cf6
manu <manuel.baden@hotmail.de>
2024-03-10 16:37:05 +0100

    fix parse method

--- src/utils/reconHelper.js
+++ src/utils/reconHelper.js
@@ -14,25 +14,21 @@ const extractVerbindungsabschnitteRecon = (rawRecon) => {
 }
 
 const compactFormatToDate = (compactDate) => {
-    const year = compactDate.slice(0, 4)
-    const month = compactDate.slice(4, 6)
-    const day = compactDate.slice(6, 8)
-    const hour = compactDate.slice(8, 10)
-    const minute = compactDate.slice(10, 12)
-    return dayjs(`${year}-${month}-${day}T${hour}:${minute}`)
+    return dayjs(compactDate)
 }
 
 const parseRecon = (rawRecon) => {
     const recon = extractVerbindungsabschnitteRecon(rawRecon)
     try {
         const verbindungsabschnitte = recon
-            .split(/\$\$[0-9]+\$+\$/)
-            .filter((x) => x)
+            .split(/\$+\$[0-9]+\$+\$/)
+            .filter(x => x)
             .map((abschnitt) => {
                 const dataList = abschnitt.split('@')
-                const abfahrtsHalt = dataList[1].slice(2)
-                const ankunftsHalt = dataList[5].slice(2)
-                const timesAndTrain = dataList[8].split('$').filter((x) => x)
+                const halte = dataList.filter(item => item.startsWith("O="))
+                const abfahrtsHalt = halte[0].slice(2)
+                const ankunftsHalt = halte[1].slice(2)
+                const timesAndTrain = dataList[dataList.length - 1].split('$').filter((x) => x)
                 const abfahrtsZeit = compactFormatToDate(timesAndTrain[0])
                 const ankunftsZeit = compactFormatToDate(timesAndTrain[1])
                 const zug = timesAndTrain[2] ? timesAndTrain[2].replace(/\s+/g, ' ') : null
