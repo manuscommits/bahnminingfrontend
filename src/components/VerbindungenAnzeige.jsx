62b04713acc47ca665489769600a37b55ab18c35
manu <manuel.baden@hotmail.de>
2024-03-10 22:06:19 +0100

    add abfahrt and ankunft halt

--- src/components/VerbindungenAnzeige.jsx
+++ src/components/VerbindungenAnzeige.jsx
@@ -1,14 +1,17 @@
-import { Stack } from "@mui/material"
+import { Stack, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
 import VerbindungAnzeige from "./VerbindungAnzeige"
 
 const VerbindungenAnzeige = () => {
-    const response = useSelector((state) => state.verbindungsanfrage.response) || {}
+    const { abfahrtsHalt, ankunftsHalt, response } = useSelector((state) => state.verbindungsanfrage) || {}
+
+    if (!response) return
     const verbindungen = response["verbindungen"] || []
     const recons = verbindungen.map(verbindung => verbindung["ctxRecon"])
 
     return (
-        <Stack direction="column">
+        <Stack direction="column" marginTop={3}>
+            <Typography textAlign="center">{abfahrtsHalt["name"] + " - " + ankunftsHalt["name"]}</Typography>
             {recons.map(recon =>
                 <VerbindungAnzeige recon={recon} key={recon} />
             )}
