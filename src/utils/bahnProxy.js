7ed2373fa8e831a19fa83f02008d6eab8eb033b6
manu <manuel.baden@hotmail.de>
2024-03-10 11:32:01 +0100

    refactor bahnProxy

--- src/utils/bahnProxy.js
+++ src/utils/bahnProxy.js
@@ -1,20 +1,11 @@
-const proxyUrl = "http://localhost:4000/bahnProxy"
+import { postFetch } from "./fetcher"
 
-const fetchWithProxy = (url, params) => {
+const fetchWithProxy = async (url, params) => {
     const body = { url }
     if (params) {
         body["options"] = params
     }
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
-    }
-    console.log("Fetch: " + proxyUrl)
-    return fetch(proxyUrl, requestOptions)
-        .then(response => response.json())
+    return await postFetch("/bahnProxy", body)
 }
 
 export default fetchWithProxy
