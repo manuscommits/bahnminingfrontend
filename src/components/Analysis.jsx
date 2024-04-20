281d6319e2cdc273b65ff61aabdc95d988795384
manu <manuel.baden@hotmail.de>
2024-04-20 17:37:11 +0200

    fix chart

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -2,20 +2,25 @@ import { Box } from "@mui/material"
 import { useSelector } from "react-redux"
 import useReconPriceHistory from "../hooks/useReconPriceHistory"
 import Chart from "./Chart"
+import dayjs from "dayjs"
 
 const Analysis = () => {
     const selectedRecon = useSelector(state => state.verbindungsanfrage.selectedRecon)
     const reconPriceHistory = useReconPriceHistory(selectedRecon)
 
     if (!selectedRecon) return
-    if (!reconPriceHistory || !reconPriceHistory.priceHistory) return <Box>Keine Daten!</Box>
-    const priceHistory = reconPriceHistory.priceHistory
-    const xAxisValues = priceHistory.map(entry => entry["t"] * 1000)
-    const yAxisValues = priceHistory.map(entry => entry["p"])
+    if (!reconPriceHistory || !reconPriceHistory["a"] || !reconPriceHistory["h"]) return <Box>Keine Daten!</Box>
+    const startDate = reconPriceHistory["a"]
+    const priceHistory = reconPriceHistory["h"]
 
+    const xAxisValues = [dayjs(startDate).unix() * 1000]
+    Array(priceHistory.length - 1).fill(null).forEach(_ => {
+        xAxisValues.push(dayjs(xAxisValues[xAxisValues.length - 1]).add(1, "day").unix() * 1000)
+    })
+    
     return (
         <Box>
-            <Chart xAxisValues={xAxisValues} yAxisValues={yAxisValues} />
+            <Chart xAxisValues={xAxisValues} yAxisValues={priceHistory} />
         </Box>
     )
 }
